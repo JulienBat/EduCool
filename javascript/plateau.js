8071a9e671085937a32aec30cdaf3096b3dab411
Julien BATAILLE <julien.bataille@telecomnancy.net>
2018-03-15 15:25:26 +0100

    plateau sur le chemin du fonctionnement

---
+++ javascript/plateau.js
@@ -0,0 +1,80 @@
+var p1 = document.getElementById("p1");
+var p2 = document.getElementById("p2");
+var p3 = document.getElementById("p3");
+var p4 = document.getElementById("p4");
+var p5 = document.getElementById("p5");
+var p6 = document.getElementById("p6");
+var p7 = document.getElementById("p7");
+var p8 = document.getElementById("p8");
+var p9 = document.getElementById("p9");
+var p10 = document.getElementById("p10");
+var p11 = document.getElementById("p11");
+var p12 = document.getElementById("p12");
+var p13 = document.getElementById("p13");
+var p14 = document.getElementById("p14");
+var p15 = document.getElementById("p15");
+var p16 = document.getElementById("p16");
+var p17 = document.getElementById("p17");
+var p18 = document.getElementById("p18");
+var p18 = document.getElementById("p19");
+var p20 = document.getElementById("p20");
+var p21 = document.getElementById("p21");
+var p22 = document.getElementById("p22");
+var p23 = document.getElementById("p23");
+var p24 = document.getElementById("p24");
+
+p1.addEventListener("click", function() {
+    p1.style.opacity = "0.5";
+}, false);
+
+p2.addEventListener("click", function() {
+    p2.style.opacity = "0.5";
+}, false);
+
+p3.addEventListener("click", function() {
+    p3.style.opacity = "0.5";
+}, false);
+
+p4.addEventListener("click", function() {
+    p4.style.opacity = "0.5";
+}, false);
+
+p5.addEventListener("click", function() {
+    p5.style.opacity = "0.5";
+}, false);
+
+p6.addEventListener("click", function() {
+    p6.style.opacity = "0.5";
+}, false);
+
+p7.addEventListener("click", function() {
+    p7.style.opacity = "0.5";
+}, false);
+
+p7.addEventListener("click", function() {
+    p7.style.opacity = "0.5";
+}, false);
+
+p8.addEventListener("click", function() {
+    p8.style.opacity = "0.5";
+}, false);
+
+p9.addEventListener("click", function() {
+    p9.style.opacity = "0.5";
+}, false);
+
+p10.addEventListener("click", function() {
+    p10.style.opacity = "0.5";
+}, false);
+
+p11.addEventListener("click", function() {
+    p11.style.opacity = "0.5";
+}, false);
+
+p12.addEventListener("click", function() {
+    p12.style.opacity = "0.5";
+}, false);
+
+p13.addEventListener("click", function() {
+    p13.style.opacity = "0.5";
+}, false);
