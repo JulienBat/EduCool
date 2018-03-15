5b7fdc527162555fd0605995915496adeee7c186
Julien BATAILLE <julien.bataille@telecomnancy.net>
2018-03-15 16:06:33 +0100

    selection et deselection ok

--- javascript/plateau.js
+++ javascript/plateau.js
@@ -1,3 +1,7 @@
+//var p = new Array(24);
+//for(var i = 0; i < 24; i++){
+//    p[i]=document.getElementById("p"+i);
+//}
 var p1 = document.getElementById("p1");
 var p2 = document.getElementById("p2");
 var p3 = document.getElementById("p3");
@@ -16,65 +20,22 @@ var p15 = document.getElementById("p15");
 var p16 = document.getElementById("p16");
 var p17 = document.getElementById("p17");
 var p18 = document.getElementById("p18");
-var p18 = document.getElementById("p19");
+var p19 = document.getElementById("p19");
 var p20 = document.getElementById("p20");
 var p21 = document.getElementById("p21");
 var p22 = document.getElementById("p22");
 var p23 = document.getElementById("p23");
 var p24 = document.getElementById("p24");
+var p = new Array(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24);
 
-p1.addEventListener("click", function() {
-    p1.style.opacity = "0.5";
-}, false);
 
-p2.addEventListener("click", function() {
-    p2.style.opacity = "0.5";
-}, false);
-
-p3.addEventListener("click", function() {
-    p3.style.opacity = "0.5";
-}, false);
-
-p4.addEventListener("click", function() {
-    p4.style.opacity = "0.5";
-}, false);
-
-p5.addEventListener("click", function() {
-    p5.style.opacity = "0.5";
-}, false);
-
-p6.addEventListener("click", function() {
-    p6.style.opacity = "0.5";
-}, false);
-
-p7.addEventListener("click", function() {
-    p7.style.opacity = "0.5";
-}, false);
-
-p7.addEventListener("click", function() {
-    p7.style.opacity = "0.5";
-}, false);
-
-p8.addEventListener("click", function() {
-    p8.style.opacity = "0.5";
-}, false);
-
-p9.addEventListener("click", function() {
-    p9.style.opacity = "0.5";
-}, false);
-
-p10.addEventListener("click", function() {
-    p10.style.opacity = "0.5";
-}, false);
-
-p11.addEventListener("click", function() {
-    p11.style.opacity = "0.5";
-}, false);
-
-p12.addEventListener("click", function() {
-    p12.style.opacity = "0.5";
-}, false);
-
-p13.addEventListener("click", function() {
-    p13.style.opacity = "0.5";
-}, false);
+for(var i = 0; i < 24; i++){
+    p[i].style.opacity = "1";
+    p[i].addEventListener("click", function() {
+        if(this.style.opacity == "1"){
+            this.style.opacity = "0.5";
+        }else{
+            this.style.opacity = "1";
+        }
+    }, false);
+}
